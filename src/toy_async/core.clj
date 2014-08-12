18d5bce74696fed0f4bbebb4b5747be60df095f7
Mike Blume <michael.blume@climate.com>
2014-08-12 15:05:26 -0700

    use http-kit

--- src/toy_async/core.clj
+++ src/toy_async/core.clj
@@ -1,8 +1,8 @@
 (ns toy-async.core
   (:require [clojure.core.async :refer [go >! <! timeout chan <!! close! >!!]]
             [cheshire.core :refer [generate-string]]
-            [ring.adapter.jetty-async :refer [run-jetty-async]]
-            [org.httpkit.client :as http]))
+            [org.httpkit.client :as http]
+            [org.httpkit.server :refer [send! with-channel run-server]]))
 
 (defn get-chan [url]
   (let [c (chan)]
@@ -14,39 +14,31 @@
         (close! c)))
     c))
 
-(count (Thread/getAllStackTraces))
-
 (defn threads [r]
   {:body (-> (Thread/getAllStackTraces)
              count
              (str \newline))})
 
 (defn foo [r]
-  (let [c (chan)]
+  (with-channel r c
     (go
       (<! (timeout 3000))
-      (>! c "foo")
-      (close! c))
-    {:body c}))
+      (send! c {:body "foo"}))))
 
 (defn bar [r]
-  (let [c (chan)]
+  (with-channel r c
     (go
       (<! (timeout 5000))
-      (>! c "bar")
-      (close! c))
-    {:body c}))
+      (send! c {:body "bar"}))))
 
 (defn foobar [r]
-  (let [c (chan)]
+  (with-channel r c
     (go
       (let [foo-chan (get-chan "http://localhost:8000/foo")
             bar-chan (get-chan "http://localhost:8000/bar")
             res (str (:body (<! foo-chan))
                      (:body (<! bar-chan)))]
-        (>! c res)
-        (close! c)))
-    {:body c}))
+        (send! c {:body res})))))
 
 (defn handler [request]
   (let [uri (:uri request)
@@ -57,9 +49,4 @@
                      (constantly {:body "not found"}))]
     (dispatch request)))
 
-(generate-string {})
-
-(defn start []
-  (run-jetty-async #'handler {:join? false :port 8000}))
-
-(start)
+(defn -main [] (run-server #'handler {:port 8000}))
